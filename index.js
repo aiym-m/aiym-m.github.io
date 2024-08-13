b13ec728271cef5997e8f85077baaa1692ed6e0b
Aiymkan <metinova.a@gmail.com>
2024-08-13 14:51:14 -0500

    all added changes

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ for (let index = 0; index <= 10; index++) {
     result = sum + index;
     
 }
+console.log(result)
 
+const button = document.querySelector(".button");
+button.addEventListener("click", function(){
+    window.open("https://github.com/aiym-m/aiym-m.github.io")
+})
