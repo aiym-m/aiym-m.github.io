28b6ab6e6891db5d9468841f0628bc85aba7b52c
Aiymkan <metinova.a@gmail.com>
2024-08-15 00:08:12 -0500

    added changes

--- homework-1/index.js
+++ homework-1/index.js
@@ -2,12 +2,12 @@ const toDoList = [
     {
         id: 1,
         task: "Buy groceries",
-        status: "ToDo"
+        status: "To Do"
     },
     {
         id: 2,
         task: "Send Emails",
-        status: "inProgress"
+        status: "In Progress"
     },
     {
         id: 3,
@@ -17,35 +17,59 @@ const toDoList = [
     {
         id: 4,
         task: "Bake Cake",
-        status: "ToDo"
+        status: "To Do"
     },
     {
         id: 5,
         task: "Write Blog Post",
-        status: "inProgress"
+        status: "In Progress"
     },
     {
         id: 6,
         task: "Do Laundry",
-        status: "ToDo"
+        status: "To Do"
     },
 ]
 
-const container = document.querySelector("ul");
-const li = document.createElement("li")
-document.body.append("li")
-toDoList.forEach(tasks => {
-    const block = `<li>
-    <p>${tasks.id}.</p>
-    <p>${tasks.task}</p>
-    <p class="status">${tasks.status}</p><button>Delete</button>
-    </li>`
-    console.log(block);
-    container.insertAdjacentHTML("beforeend", block)
-})
 
-const inputEl = document.querySelector("input");
-const buttonEl = document.querySelector(".addButton")
+
+const container = document.querySelector("ul");
+function renderToDoList() {
+    container.innerHTML = "";
+    toDoList.forEach(tasks => {
+        const block = `<li>
+        <p>${tasks.id}.</p>
+        <p>${tasks.task}</p>
+        <p class="status">${tasks.status}</p><button id="${tasks.id}">Delete</button>
+        </li>`
+        console.log(block);
+        container.insertAdjacentHTML("beforeend", block)
+    })
+    document.querySelectorAll("li button").forEach((button, index) => {
+        button.addEventListener("click", function(){
+            toDoList.splice(index, 1);
+            renderToDoList()
+    
+        })
+    })
+    
+} 
+renderToDoList();
+
+
+const buttonEl = document.querySelector(".addButton");
+buttonEl.addEventListener("click", function(){
+    const inputEl = document.querySelector("input");
+    const newTask = {
+        id: toDoList.length + 1,
+        task: inputEl.value,
+        status: "To Do",
+    }
+    toDoList.push(newTask);
+    renderToDoList();
+    inputEl.value = "";
+}) 
+
 
 
 
